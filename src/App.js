95cbc1dc5c6f99c0c7bbd4584aa0b9706ff79586
unknown <moshbidon@gmail.com>
2023-10-19 19:57:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+import React, {useState, useRef} from 'react'
+import { FiPlayCircle, FiPauseCircle, FiRotateCcw } from "react-icons/fi";
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [theme, setTheme] = useState('worktime');
+  const [count, setCount] = useState(1500);
+  const timeRef = useRef(null);
+  
+  const startCount = () => {
+      if(!timeRef.current) {
+      timeRef.current = setInterval(changingTime, 1000);
+      }
+  }
+
+  const pauseCount = () => {
+      clearInterval(timeRef.current);
+      timeRef.current = false;
+  }
+
+  const resetTime = () => {
+      clearInterval(timeRef.current);
+      setCount(1500);
+      timeRef.current = false;
+  }
+  
+  const changingTime = () => setCount((state) => {
+     if (state === 1) {clearInterval(timeRef.current)}
+     return state - 1;
+  })
+  
+  const renderingTime = () => {
+      const minutes = Math.floor(count / 60);
+      const seconds = count % 60;
+      let finalCountdown; 
+      count % 60 >= 10 ? 
+          finalCountdown = `${minutes}:${seconds}` : 
+          finalCountdown = `${minutes}:0${seconds}`;       
+      console.log(finalCountdown, count);
+      return finalCountdown;
+      
+  }
+
+  return (   
+      <div className={`container ${theme === 'worktime' ?
+      'worktime' : 'chilltime'}`}>
+      <h1>Pomodorro Timer</h1>
+        <div className='painel'>
+          <p onClick={()=>{setTheme('worktime');
+                          setCount(1500);
+                          pauseCount()}} 
+                          id='work' 
+                          className="active">Work</p>
+          <p onClick={()=>{setTheme('chilltime'); 
+                          setCount(300);
+                          pauseCount()}} 
+                          id='break'>Chill</p>
+        </div>
+        <section>
+                <div className="container">
+                <div className="timer">
+                    <div className="circle">
+                        <div className="time">
+                            <p className="countdown">{renderingTime()}</p>
+                        </div>
+                    </div>
+                </div>
+                        <div className="controls">
+                            <button onClick={startCount} id="start"><FiPlayCircle className='icon'/></button>
+                            <button onClick={pauseCount} id="pause"><FiPauseCircle className='icon'/></button>
+                            <button onClick={() => {resetTime(); setTheme('worktime')}} id="restart"><FiRotateCcw className='icon'/></button>
+                        </div>
+                </div>    
+            </section>
+      </div>
   );
 }
 
